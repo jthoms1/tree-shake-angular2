fe818b054cd06d79ee09129dcabfb819c848db7d
Josh Thomas <josh@ionic.io>
2016-06-09 09:08:04 -0500

    Adding typescript compiling using jsnext.

---
+++ typescript-with-jsnext/src/vendor.ts
@@ -0,0 +1,4 @@
+import '@angular/platform-browser-dynamic';
+import '@angular/common';
+import '@angular/core';
+import 'rxjs/add/operator/map';
---
+++ typescript-with-jsnext/webpack.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var webpack = require('webpack');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var path = require('path');
+var nodeEnv = 'development';
+
+module.exports = {
+  entry: {
+    polyfills: './src/polyfills',
+    vendor: './src/vendor',
+    main: './src/main',
+  },
+  output: {
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    mainFields: ['jsnext:main', 'main', 'browser']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }}),
+    new CommonsChunkPlugin({ name: ['polyfills', 'vendor', 'main'].reverse(), minChunks: Infinity })
+  ]
+}
